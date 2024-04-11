31b4afb1e2099564ad994bda67835b9cfaf6740e
Gustavo Carvalho <gcrodrigues413@gmail.com>
2024-04-11 04:04:28 -0300

    feat: theme storybook

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,13 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: '{ multiexperience }',
+
+  brandUrl: 'https://www.multiexperience.com.br',
+  brandImage: './images/mx-logo.png',
+  brandTarget: '_self',
+  colorPrimary: '#1cf7f7',
+  colorSecondary: '#ff004f',
+  barHoverColor: '#ff004f',
+})
